c94fc7f94186d51c52c30cef722d8e766d0c5478
Talha Ghauri <talhaghauridev@gmail.com>
2025-09-17 13:29:15 +0500

    add chat group route

---
+++ src/app/(chat)/layout.tsx
@@ -0,0 +1,37 @@
+import { SidebarToggle } from '@/components/shared/sidebar-toggle';
+import AppSidebar from '@/components/sidebar';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import { STORAGE_KEYS } from '@/constants/storage-keys';
+import { cookies } from 'next/headers';
+import React from 'react';
+
+function getUserFromCookies(userCookie: any) {
+  if (userCookie) {
+    try {
+      return JSON.parse(userCookie);
+    } catch {
+      return null;
+    }
+  }
+  return null;
+}
+
+export default async function SidebarLayout({ children }: { children: React.ReactNode }) {
+  const cookieStore = await cookies();
+  const isCollapsed = cookieStore.get(STORAGE_KEYS.SIDEBAR_COOKIE_NAME)?.value !== 'true';
+  const user = getUserFromCookies(cookieStore.get(STORAGE_KEYS.AUTH.USER_DATA)?.value);
+
+  return (
+    <SidebarProvider defaultOpen={!isCollapsed}>
+      <AppSidebar />
+      <SidebarInset>
+        <div className="flex items-center gap-2 md:hidden">
+          <span className="px-2 py-2">
+            <SidebarToggle />
+          </span>
+        </div>
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
